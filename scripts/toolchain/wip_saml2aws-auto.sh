bb21581ae846f7b4c7a972c9990093834b30ab96
EC2 Default User <ec2-user@ip-10-0-0-105.eu-west-1.compute.internal>
2018-10-24 14:30:45 +0000

    added link for short name s2a

--- scripts/toolchain/wip_saml2aws-auto.sh
+++ scripts/toolchain/wip_saml2aws-auto.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+. ./file_append.sh
 . ./colors.sh
 
 # https://github.com/Rukenshia/saml2aws-auto/releases
@@ -9,7 +10,10 @@ mkdir $HOME/.local/bin -p
 wget https://github.com/Rukenshia/saml2aws-auto/releases/download/$version/saml2aws-auto-$version-x86_64-unknown-linux-gnu.tar.gz
 
 tar zxf saml2aws-auto-$version-x86_64-unknown-linux-gnu.tar.gz -C $HOME/.local/bin
+ln -s $HOME/.local/bin/saml2aws-auto $HOME/.local/bin/s2a -f
 
 rm saml2aws-auto*.gz*
 
 file_append 'PATH=$PATH:$HOME/.local/bin' ~/.bashrc
+
+. ~/.bashrc

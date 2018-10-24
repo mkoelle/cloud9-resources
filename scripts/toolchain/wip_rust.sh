8af64ea57fe4172f73a65c009d8e7f85ab34f889
EC2 Default User <ec2-user@ip-10-0-0-105.eu-west-1.compute.internal>
2018-10-24 14:39:24 +0000

    removed wip files from master

--- scripts/toolchain/wip_rust.sh
+++
@@ -1,3 +0,0 @@
-curl https://sh.rustup.rs -sSf | sh
-
-sudo yum install gmp-devel.x86_64
--- scripts/toolchain/wip_saml2aws-auto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-. ./colors.sh
-
-# https://github.com/Rukenshia/saml2aws-auto/releases
-version=1.7.1-rc.5
-
-wget https://github.com/Rukenshia/saml2aws-auto/releases/download/$version/saml2aws-auto-$version-x86_64-unknown-linux-gnu.tar.gz
-
-tar zxf saml2aws-auto-$version-x86_64-unknown-linux-gnu.tar.gz -C $HOME/.local/bin

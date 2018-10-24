7ce30da283238809c96b5d596373eb62f012cd0b
EC2 Default User <ec2-user@ip-10-0-0-105.eu-west-1.compute.internal>
2018-10-24 14:11:23 +0000

    updated saml2aws-auto version

--- scripts/toolchain/wip_saml2aws-auto.sh
+++ scripts/toolchain/wip_saml2aws-auto.sh
@@ -1,5 +1,9 @@
-wget https://github.com/Rukenshia/saml2aws-auto/releases/download/1.6.3/saml2aws-auto-1.6.3-x86_64-unknown-linux-gnu.tar.gz
+#!/bin/bash
+. ./colors.sh
 
-tar zxf saml2aws-auto-1.6.3-x86_64-unknown-linux-gnu.tar.gz -C $HOME/.local/bin
+# https://github.com/Rukenshia/saml2aws-auto/releases
+version=1.7.1-rc.5
 
-#rm -f saml2aws-auto-1.6.3-x86_64-unknown-linux-gnu.tar.gz
+wget https://github.com/Rukenshia/saml2aws-auto/releases/download/$version/saml2aws-auto-$version-x86_64-unknown-linux-gnu.tar.gz
+
+tar zxf saml2aws-auto-$version-x86_64-unknown-linux-gnu.tar.gz -C $HOME/.local/bin

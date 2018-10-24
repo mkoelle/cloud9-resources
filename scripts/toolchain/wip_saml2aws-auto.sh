d3a71f0f1e2c1fd3ac5c296e51949aa615c83995
EC2 Default User <ec2-user@ip-10-0-0-105.eu-west-1.compute.internal>
2018-10-24 14:24:05 +0000

    moved s2a to bin folder, added folder to path

--- scripts/toolchain/wip_saml2aws-auto.sh
+++ scripts/toolchain/wip_saml2aws-auto.sh
@@ -4,6 +4,12 @@
 # https://github.com/Rukenshia/saml2aws-auto/releases
 version=1.7.1-rc.5
 
+mkdir $HOME/.local/bin -p
+
 wget https://github.com/Rukenshia/saml2aws-auto/releases/download/$version/saml2aws-auto-$version-x86_64-unknown-linux-gnu.tar.gz
 
 tar zxf saml2aws-auto-$version-x86_64-unknown-linux-gnu.tar.gz -C $HOME/.local/bin
+
+rm saml2aws-auto*.gz*
+
+file_append 'PATH=$PATH:$HOME/.local/bin' ~/.bashrc

d94918e1dc27445adebec78fc1818a8bf050f81a
EC2 Default User <ec2-user@ip-10-0-0-105.eu-west-1.compute.internal>
2018-10-24 12:26:27 +0000

    more playing with colors and redunant output

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 . ./colors.sh
 
-echo -e "${LGREEN}Installing${CYAN} NPM${NC}"
-npm --version
+echo -e "${LGREEN}Updating${CYAN} NPM${NC}"
+echo -e "${LPURPLE}Current Version:"; npm --version; echo -e "${NC}"
 npm install -g npm
-npm --version
+echo -e "${LPURPLE}New Version:"; npm --version; echo -e "${NC}"
 
 echo -e "${LGREEN}Installing${CYAN} NVM${NC}"
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 . ~/.bash_profile
 
 echo -e "${LGREEN}Updating${CYAN} Node${NC}"
+echo -e "${LPURPLE}Current Version:"; node --version; echo -e "${NC}"
 nvm install node
 nvm alias default stable
-node --version
+echo -e "${LPURPLE}New Version:"; node --version; echo -e "${NC}"
 
 echo -e "${LGREEN}Installing${CYAN} C9-cli${NC}"
 npm install -g c9

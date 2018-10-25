9e4f449c8294860e2053db9fe72dd37d629c8e3a
Mark Koelle <mark.koelle@digitalunit.berlin>
2018-10-25 10:50:23 +0000

    added pip3 allias

--- scripts/toolchain/python3.sh
+++ scripts/toolchain/python3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+. ./file_append.sh
 . ./colors.sh
 
 # https://docs.aws.amazon.com/cloud9/latest/user-guide/sample-python.html
@@ -10,6 +11,9 @@ sudo update-alternatives --set python '/usr/bin/python3.6'
 
 # refresh environment variables in script
 set -a; source /etc/environment; set +a;
+
+file_append 'alias pip3="python3 -m pip"' ~/.bashrc
+. ~/.bash_profile
 
 echo -e "${LPURPLE}New Python Version:"; python --version; echo -e "${NC}"
 echo -e "${LPURPLE}New Pip Version:"; pip --version; echo -e "${NC}"
